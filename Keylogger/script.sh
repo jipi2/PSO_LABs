2827e7156a968685b7e8d455c012cf7147c17de5
jipi2 <jipianu_mihnea@yahoo.com>
2022-12-24 20:18:00 +0200

    update

--- Keylogger/script.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-python3 EventsPython.py | egrep -o "event[0-9+]" > temp.txt
-eventName=`head -n1 temp.txt`
-rm temp.txt
-gcc readEvents.c -o readEvents
-echo "$eventName" > eventNumber.txt
-rm store.txt 2> /dev/null
-touch store.txt
-./readEvents eventNumber.txt &

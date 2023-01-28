80ddcdadf5a7457f1a9ff01a418f1e28849f0a8b
jipi2 <100122693+jipi2@users.noreply.github.com>
2023-01-28 22:41:04 +0200

    Update server.c

--- TEMA_3/server.c
+++ TEMA_3/server.c
@@ -17,6 +17,7 @@
 #define BUFF_SIZE 4096
 #define OK 1
 #define N_OK -1
+#define MAX_FILES_NUMBER 100
 
 #define LIST "list"
 #define BYE "bye"
@@ -34,17 +35,112 @@ pthread_key_t key;
 pthread_t threadIds[10];
 int thread_count = 0;
 pthread_mutex_t counterMutex;
+pthread_mutex_t modifyStructMutex;
+
+struct FilesStruct
+{
+    char **files;
+    int *counter;
+    pthread_mutex_t *counter_lock;
+    pthread_mutex_t *write_lock;
+    int size;
+};
+
+struct FilesStruct filesInUse;
+
+int isFileInUse(char *filename)
+{
+    for (int i = 0; i < filesInUse.size; i++)
+        if (strcmp(filename, filesInUse.files[i]) == 0)
+            return 1;
+    return 0;
+}
+
+void add_file_in_FilesInUse(char *filename)
+{
+    filesInUse.files[filesInUse.size] = strdup(filename);
+    pthread_mutex_init(&filesInUse.counter_lock[filesInUse.size], NULL);
+    pthread_mutex_init(&filesInUse.write_lock[filesInUse.size], NULL);
+    filesInUse.counter[filesInUse.size] = 0;
+    filesInUse.size++;
+}
+
+int get_Index_of_file_in_use(char *filename)
+{
+     for (int i = 0; i < filesInUse.size; i++)
+        if (strcmp(filename, filesInUse.files[i]) == 0)
+            return i;
+    
+    pthread_mutex_lock(&modifyStructMutex);
+    add_file_in_FilesInUse(filename);
+    pthread_mutex_unlock(&modifyStructMutex);
+
+    return (filesInUse.size-1);
+}
+
+void init_FilesStructure()
+{
+    filesInUse.files = (char**)malloc(sizeof(char*)*MAX_FILES_NUMBER);
+    filesInUse.counter = (int*)malloc(sizeof(int)*MAX_FILES_NUMBER);
+    filesInUse.counter_lock = (pthread_mutex_t *)malloc(sizeof(pthread_mutex_t)*MAX_FILES_NUMBER);
+    filesInUse.write_lock = (pthread_mutex_t *)malloc(sizeof(pthread_mutex_t)*MAX_FILES_NUMBER);
+    filesInUse.size = 0;
+}
+
+void destroy_FilesInUseStructure()
+{
+    int i=0;
+    for(i=0;i<filesInUse.size;i++)
+        free(filesInUse.files[i]);
+    free(filesInUse.files);
+    free(filesInUse.counter_lock);
+    free(filesInUse.write_lock);
+    free(filesInUse.counter);
+}
+
+void lock_for_writing(pthread_mutex_t *writeMutex)
+{
+    pthread_mutex_lock(writeMutex);
+}
+
+void unlock_for_writing(pthread_mutex_t *writeMutex)
+{
+    pthread_mutex_unlock(writeMutex);
+}
+
+void lock_for_reading(pthread_mutex_t *counterMutex, pthread_mutex_t *writeMutex, int *counter)
+{
+    pthread_mutex_lock(counterMutex);
+    (*counter)++;
+    if ((*counter) == 1)
+    {
+        pthread_mutex_lock(writeMutex);
+    }
+    pthread_mutex_unlock(counterMutex);
+}
+
+void unlock_for_reading(pthread_mutex_t *counterMutex, pthread_mutex_t *writeMutex, int *counter)
+{
+    pthread_mutex_lock(counterMutex);
+    *(counter)--;
+    if ((*counter) == 0)
+    {
+        pthread_mutex_unlock(writeMutex);
+    }
+    pthread_mutex_unlock(counterMutex);
+}
 
 void init_mutexes()
 {
     pthread_mutex_init(&counterMutex, NULL);
+    pthread_mutex_init(&modifyStructMutex, NULL);
 }
 
 void destroy_mutexes()
 {
     pthread_mutex_destroy(&counterMutex);
+    pthread_mutex_destroy(&modifyStructMutex);
 }
-
 
 char *ls_func()
 {
@@ -104,16 +200,15 @@ void ls_send(char *command, int *sock)
 
     printf("%s\n", command);
 
-    strcpy(answ,"");
+    strcpy(answ, "");
 
-    sprintf(status,"%d",OK);
+    sprintf(status, "%d", OK);
     strcat(answ, status);
     strcat(answ, ";");
 
     len = strlen(files);
-    sprintf(number,"%d",len);
-    strcat(answ,number);
-
+    sprintf(number, "%d", len);
+    strcat(answ, number);
 
     len = strlen(answ);
 
@@ -121,18 +216,18 @@ void ls_send(char *command, int *sock)
 
     len = strlen(answ);
     format_str(answ);
-    
+
     clientSocket = *sock;
 
     rt = write(clientSocket, answ, len);
-    if(rt < 0)
+    if (rt < 0)
     {
         printf("EROARE\n");
         return;
     }
-    
+
     free(files);
-    //free(answ);
+    // free(answ);
 }
 
 void send_file(char *filename, int *client)
@@ -141,15 +236,14 @@ void send_file(char *filename, int *client)
     int status = 0;
     int fd = 0;
     off_t bytesWrote = 0;
-    int n=0;
-    
+    int n = 0;
 
     fd = open(filename, O_RDONLY);
-    if(fd < 0)
+    if (fd < 0)
     {
         status = 0;
-        write(*client, &status,sizeof(int));
-        return ;
+        write(*client, &status, sizeof(int));
+        return;
     }
     else
     {
@@ -160,12 +254,11 @@ void send_file(char *filename, int *client)
     size = lseek(fd, 0, SEEK_END);
     write(*client, &size, sizeof(int));
 
-    while(bytesWrote < size)
+    while (bytesWrote < size)
     {
         n = sendfile(*client, fd, &bytesWrote, size);
         bytesWrote += n;
     }
-
 }
 
 char *getFileName(char *command)
@@ -175,32 +268,35 @@ char *getFileName(char *command)
     char delim[] = ";:\n ";
     int numBytes = 0;
 
-    p=strtok(command, delim);
-    p=strtok(NULL, delim);
+    p = strtok(command, delim);
+    p = strtok(NULL, delim);
     numBytes = atoi(p);
 
-    fileName = (char*)malloc(sizeof(char)*numBytes);
+    fileName = (char *)malloc(sizeof(char) * numBytes);
 
+    p = strtok(NULL, delim);
 
-    p=strtok(NULL, delim);
-
-    if(strlen(p)>numBytes)
+    if (strlen(p) > numBytes)
         return NULL;
 
     strcpy(fileName, p);
     return fileName;
 }
 
-void get_funct(char *command, int *client)  
+void get_funct(char *command, int *client)
 {
-    //comanda clientului este separata prin ";"
+    // comanda clientului este separata prin ";"
     char *fileName;
+    int index = 0;
 
     printf("%s\n", command);
 
     fileName = getFileName(command);
 
+    index = get_Index_of_file_in_use(fileName); 
+    lock_for_reading(&filesInUse.counter_lock[index], &filesInUse.write_lock[index], &filesInUse.counter[index]);
     send_file(fileName, client);
+    unlock_for_reading(&filesInUse.counter_lock[index], &filesInUse.write_lock[index], &filesInUse.counter[index]);
 
     free(fileName);
 }
@@ -211,32 +307,32 @@ int delete_file(char *fileName)
     DIR *dir;
     struct dirent *entry;
 
-    if(fileName == NULL)
+    if (fileName == NULL)
     {
         return status;
     }
 
-    if(strcmp(fileName,".") == 0 || strcmp(fileName,"..") == 0 || strcmp(fileName, "server") == 0 ||
-                strcmp(fileName, "server.c") == 0 || strcmp(fileName, "client") == 0 || strcmp(fileName, "client.c") == 0 || strcmp(fileName, "Makefile") == 0)
+    if (strcmp(fileName, ".") == 0 || strcmp(fileName, "..") == 0 || strcmp(fileName, "server") == 0 ||
+        strcmp(fileName, "server.c") == 0 || strcmp(fileName, "client") == 0 || strcmp(fileName, "client.c") == 0 || strcmp(fileName, "Makefile") == 0)
     {
         return 0;
     }
 
     dir = opendir(".");
-    if (dir != NULL) 
+    if (dir != NULL)
     {
-        while ((entry = readdir(dir)) != NULL) 
+        while ((entry = readdir(dir)) != NULL)
         {
-            if (entry->d_type == DT_REG) 
+            if (entry->d_type == DT_REG)
             {
-                if (strcmp(entry->d_name, fileName) == 0) 
+                if (strcmp(entry->d_name, fileName) == 0)
                 {
-                    if (unlink(fileName) == 0) 
+                    if (unlink(fileName) == 0)
                     {
                         status = 1;
                         return status;
                     }
-                    else 
+                    else
                     {
                         return status;
                     }
@@ -251,7 +347,6 @@ int delete_file(char *fileName)
     }
 
     return status;
-
 }
 
 void deleteFunct(char *command, int *client)
@@ -272,20 +367,19 @@ int make_file(char *fileName, char *content, int sizeContent)
 {
     int fd;
     int bytesWrote = 0;
-    int n=0;
+    int n = 0;
 
     fd = open(fileName, O_WRONLY | O_CREAT, 0644);
-    if(fd < 0)
+    if (fd < 0)
     {
         return 0;
     }
 
-
-    while(bytesWrote < sizeContent)
+    while (bytesWrote < sizeContent)
     {
-        n = write(fd, content+bytesWrote, sizeContent - bytesWrote);
+        n = write(fd, content + bytesWrote, sizeContent - bytesWrote);
         bytesWrote += n;
-        if(n < 0)
+        if (n < 0)
         {
             close(fd);
             return 0;
@@ -307,103 +401,107 @@ void putFunct(char *command, int *client)
     char *content = NULL;
     int status = 0;
 
-    p=strtok(command, delim);
-    p=strtok(NULL,delim);
+    p = strtok(command, delim);
+    p = strtok(NULL, delim);
     sizeNameF = atoi(p);
 
-    p=strtok(NULL,delim);
-    if(strlen(p)<sizeNameF)
+    p = strtok(NULL, delim);
+    if (strlen(p) < sizeNameF)
     {
         write(*client, &status, sizeof(int));
         return;
     }
 
-    fileName = (char*)malloc(sizeof(char)*sizeNameF);
+    fileName = (char *)malloc(sizeof(char) * sizeNameF);
     strcpy(fileName, p);
 
-    p = strtok(NULL, delim); 
+    p = strtok(NULL, delim);
     sizeContent = atoi(p);
 
     p = strtok(NULL, delim);
-    content = (char*)malloc(sizeof(char)*sizeContent);
-  
+    content = (char *)malloc(sizeof(char) * sizeContent);
+
     strncpy(content, p, sizeContent);
-    
+
     content[sizeContent] = '\0';
 
     printf("%s\n", fileName);
     printf("%d\n", sizeContent);
     printf("%s\n", content);
-    
+
     status = make_file(fileName, content, sizeContent);
 
     write(*client, &status, sizeof(int));
 
     free(fileName);
     free(content);
-    
 }
 
 char *getOpcode(char *command)
 {
     char *aux = NULL;
-    int i=0;
+    int i = 0;
     int size = 0;
 
-    for(i=0;i<strlen(command);i++)
+    for (i = 0; i < strlen(command); i++)
     {
         size++;
-        if(command[i] == ';')
+        if (command[i] == ';')
         {
             break;
         }
     }
 
-    aux = (char*)malloc(sizeof(char)*size);
-    strncpy(aux, command, size-1);
-    aux[size-1] = '\0';
+    aux = (char *)malloc(sizeof(char) * size);
+    strncpy(aux, command, size - 1);
+    aux[size - 1] = '\0';
 
     return aux;
 }
 
-int updateFile(char *fileName, int startByte,char *content, int sizeContent)
+int updateFile(char *fileName, int startByte, char *content, int sizeContent)
 {
     int fd;
     int initFileSize = 0;
     int firstPartSize = 0;
     int lastPartSize = 0;
     int allContentSize = 0;
-    int n=0;
+    int n = 0;
     int bytesWrote = 0;
     char *allContent = NULL;
     char *firstPart = NULL;
     char *lastPart = NULL;
+    int index = 0;
 
     fd = open(fileName, O_RDWR);
-    if(fd < 0)
+    if (fd < 0)
     {
         return 0;
     }
 
+    index = get_Index_of_file_in_use(fileName);
+    lock_for_writing(&filesInUse.write_lock[index]);
+
     initFileSize = lseek(fd, 0, SEEK_END);
-    if(startByte >= initFileSize)
+    if (startByte >= initFileSize)
     {
-        while(bytesWrote < sizeContent)
+        while (bytesWrote < sizeContent)
         {
-            n = write(fd, content+bytesWrote, sizeContent - bytesWrote);
-            bytesWrote+=n;
-            if(n<0)
+            n = write(fd, content + bytesWrote, sizeContent - bytesWrote);
+            bytesWrote += n;
+            if (n < 0)
             {
                 close(fd);
                 return 0;
             }
         }
+        lock_for_writing(&filesInUse.write_lock[index]);
         return 1;
     }
     else
     {
         lseek(fd, 0, SEEK_SET);
-        firstPart = (char*)malloc(sizeof(char)*startByte);
+        firstPart = (char *)malloc(sizeof(char) * startByte);
         read(fd, firstPart, startByte);
         firstPart[startByte] = '\0';
 
@@ -411,41 +509,39 @@ int updateFile(char *fileName, int startByte,char *content, int sizeContent)
         lseek(fd, 0, SEEK_SET);
         int n2 = lseek(fd, 0, SEEK_END);
 
-        lastPartSize = n2-n1;
+        lastPartSize = n2 - n1;
 
         lseek(fd, startByte, SEEK_SET);
-        lastPart = (char*)malloc(sizeof(char)*lastPartSize);
+        lastPart = (char *)malloc(sizeof(char) * lastPartSize);
         read(fd, lastPart, lastPartSize);
         lastPart[lastPartSize] = '\0';
 
-        allContentSize = n2+sizeContent;
+        allContentSize = n2 + sizeContent;
 
-        allContent = (char*)malloc(sizeof(char)*allContentSize);
+        allContent = (char *)malloc(sizeof(char) * allContentSize);
 
-        strcpy(allContent,firstPart);
+        strcpy(allContent, firstPart);
         strcat(allContent, content);
         strcat(allContent, lastPart);
 
         bytesWrote = 0;
-        n=0;
+        n = 0;
 
         lseek(fd, 0, SEEK_SET);
-        while(bytesWrote < allContentSize)
+        while (bytesWrote < allContentSize)
         {
-            n = write(fd, allContent+bytesWrote, allContentSize - bytesWrote);
-            bytesWrote+=n;
-            if(n<0)
+            n = write(fd, allContent + bytesWrote, allContentSize - bytesWrote);
+            bytesWrote += n;
+            if (n < 0)
             {
                 close(fd);
                 return 0;
             }
         }
         close(fd);
-
     }
+    unlock_for_writing(&filesInUse.write_lock[index]);
     return 1;
-
-
 }
 
 void updateFunct(char *command, int *client)
@@ -459,19 +555,19 @@ void updateFunct(char *command, int *client)
     int status = 0;
     int startByte = 0;
 
-    p=strtok(command, delim);
-    p=strtok(NULL,delim);
+    p = strtok(command, delim);
+    p = strtok(NULL, delim);
     sizeNameF = atoi(p);
 
-    p=strtok(NULL,delim);
-    if(strlen(p)>sizeNameF)
+    p = strtok(NULL, delim);
+    if (strlen(p) > sizeNameF)
     {
         write(*client, &status, sizeof(int));
 
         return;
     }
 
-    fileName = (char*)malloc(sizeof(char)*sizeNameF);
+    fileName = (char *)malloc(sizeof(char) * sizeNameF);
     strcpy(fileName, p);
 
     p = strtok(NULL, delim);
@@ -479,7 +575,7 @@ void updateFunct(char *command, int *client)
 
     p = strtok(NULL, delim);
     sizeContent = atoi(p);
-    content = (char*)malloc(sizeof(char)*sizeContent);
+    content = (char *)malloc(sizeof(char) * sizeContent);
     strcpy(content, "");
 
     p = strtok(NULL, delim);
@@ -487,14 +583,13 @@ void updateFunct(char *command, int *client)
     content[sizeContent] = '\0';
 
     printf("%s\n%d\n%s\n%d\n", fileName, startByte, content, sizeContent);
-    
-    status = updateFile(fileName, startByte,content, sizeContent);
+
+    status = updateFile(fileName, startByte, content, sizeContent);
 
     write(*client, &status, sizeof(status));
 
     free(fileName);
     free(content);
-
 }
 
 void *thread_client_command(void *param)
@@ -526,7 +621,7 @@ void *thread_client_command(void *param)
     //------------------------------------------------------------------------------------------------------------------
 
     dataSize = read(clientSock, command, BUFF_SIZE);
-    if(dataSize < 0)
+    if (dataSize < 0)
     {
         close(clientSock);
         free(opcode);
@@ -554,19 +649,19 @@ void *thread_client_command(void *param)
     {
         deleteFunct(command, &clientSock);
     }
-    else if(strcmp(opcode, PUT) == 0)
+    else if (strcmp(opcode, PUT) == 0)
     {
         putFunct(command, &clientSock);
     }
     else if (strcmp(opcode, UPDATE) == 0)
     {
+        sleep(1);
         updateFunct(command, &clientSock);
     }
-    else if(strcmp(opcode, BYE)==0)
+    else if (strcmp(opcode, BYE) == 0)
     {
         printf("%s\n", command);
     }
-
 
     close(clientSock);
     free(opcode);
@@ -580,16 +675,17 @@ void *thread_client_command(void *param)
 
 void exit_handle(int signum)
 {
-    if(signum == SIGTERM || signum == SIGINT)
+    if (signum == SIGTERM || signum == SIGINT)
     {
         printf("Graceful Termination\n");
 
-        for(int i=0;i<thread_count;i++)
+        for (int i = 0; i < thread_count; i++)
         {
-            pthread_join(threadIds[i],NULL);
+            pthread_join(threadIds[i], NULL);
         }
 
         destroy_mutexes();
+        destroy_FilesInUseStructure();
         exit(0);
     }
 }
@@ -604,7 +700,6 @@ void set_signals()
     sigaction(SIGTERM, &sa, NULL);
 }
 
-
 int main()
 {
     int listenSocket, connectSocket;
@@ -614,6 +709,7 @@ int main()
 
     set_signals();
     init_mutexes();
+    init_FilesStructure();
     pthread_key_create(&key, NULL);
 
     bzero(&serverAddr, sizeof(serverAddr));
@@ -636,5 +732,7 @@ int main()
         pthread_create(&threadIds[thread_count++], NULL, &thread_client_command, &param);
     }
 
+    destroy_mutexes();
+    destroy_FilesInUseStructure();
     return 0;
 }
